9853c810f68b6e84b8f42cb5893d90c459f57c1a
DokkarRachidReda <hr_dokkar@esi.dz>
2020-09-28 02:18:59 +0100

    added dial intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,4 +45,16 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+
+        Intent dialIntent = new Intent(Intent.ACTION_SEND);
+
+        Uri dialUri = Uri.parse("tel:0123456789");
+
+        dialIntent.setData(dialUri);
+
+        startActivity(dialIntent);
+
+    }
 }
